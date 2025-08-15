9becaabddcd7ee936f83f68f30f1198b604aea92
NielSianipar <danielsianipar2005@gmail.com>
2025-08-16 00:06:14 +0700

    First upload
    First upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi Kalkulator</title>
+</head>
+<body>
+    <h1>Kalkulator Sederhana Daniel</h1>
+    <button><a href="tambah.php">TAMBAH</a></button>
+    <button><a href="kurang.php">KURANG</a></button>
+    <button><a href="bagi.php">BAGI</a></button>
+    <button><a href="kali.php">KALI</a></button>
+    
+</body> 
+</html>    
---
+++ kali.php
@@ -0,0 +1,33 @@
+<?php
+    $result = null;
+   if (isset($_POST['hasil'])) {
+    $x = $_POST['angka1'];
+    $y = $_POST['angka2'];
+
+    $result = $x * $y;
+   }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PROGRAM PENGKALIAN ANGKA</h1>
+    <form action="kali.php" method="POST">
+        <input type="number" placeholder="masukan angka pertama" name="angka1">
+        <input type="number" placeholder="masukan angka kedua" name="angka2" require>
+        <button type="submit" name="hasil">Hasil</button>
+    </form>
+    <?php if ($result !== null): ?>
+        <h2>Hasil: <?= htmlspecialchars($result) ?></h2>
+    <?php endif; ?>
+
+    <button><a href="index.php">Keluar</a></button>
+
+</body>
+</html>
