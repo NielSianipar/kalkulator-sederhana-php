2473849b87b18144132ccad8d71cde1c0ade9339
NielSianipar <danielsianipar2005@gmail.com>
2025-08-17 23:23:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
     <title>Aplikasi Kalkulator</title>
 </head>
 <body>
-    <h1>Kalkulator Sederhana Daniel</h1>
+    <h1>Kalkulator Sederhana Daniel PHP Native</h1>
     <button><a href="tambah.php">TAMBAH</a></button>
     <button><a href="kurang.php">KURANG</a></button>
     <button><a href="bagi.php">BAGI</a></button>
     <button><a href="kali.php">KALI</a></button>
     
 </body> 
+
 </html>    
